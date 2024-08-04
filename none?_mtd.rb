efa2d1b9c66b9e0a3986096cf2e6ffde18393ca2
samuel <samiehave9@gmail.com>
2024-08-04 23:18:51 +0300

    Using NONE? mtd

--- none?_mtd.rb
+++ none?_mtd.rb
@@ -2,6 +2,7 @@
 # As you might expect, #none? returns true only if the condition in the block matches none of the elements in your array or hash; otherwise, it returns false.
 # First, let’s see how this could be achieved using #each. You’ll notice that this approach is very similar to what we did for #all?.
 
+# Example 1
 fruits = ["apple", "banana", "strawberry", "pineapple"]
 res = false
 
@@ -17,15 +18,24 @@ end
 
 puts res # true
 
+# Example 2
 fruits2 = ["apple", "banana", "strawberry", "pineapple"]
 res2 = false
 
 fruits2.each do |fruit|
-  if fruit.length > 15
-    res = true
+  if fruit.length > 6
+    res2 = false
     break
   end
-  res2 = false
+  res2 = true
 end
 
 puts res2 # false
+
+# Using NONE? method, we can refactor the above code as follows:
+# Example 1
+fruits3 = ["apple", "banana", "strawberry", "pineapple"]
+puts fruits3.none? { |fruit| fruit.length > 10 }
+
+# Example 2
+puts fruits3.none? { |fruit| fruit.length > 6 }
