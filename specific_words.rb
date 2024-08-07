026a01f8fdfb45fb600605f7ca74e775cd103565
samuel <samiehave9@gmail.com>
2024-08-07 09:04:27 +0300

    NONE mtd

--- specific_words.rb
+++ specific_words.rb
@@ -15,4 +15,16 @@ puts all_words_have_specific_size?(words) # false
 words2 = ["bacon", "orange", "apple", "kiwi"]
 output = words2.all? { |word| word.size == 5 }
 
-puts output # true
+puts output # false
+
+# All? will return true for an empty array, since NO elements are false then all elements must be true.
+[].all? { |s| s.size == 1 } # true
+
+# NONE? method
+# if you want to reverse all? mtd, you can use none mtd
+words2 = ["banana", "pumpkin", "watermelon", "jackfruit"]
+output2 = words2.none? { |word| word.size == 5 }
+output2 = words2.all? { |word| word.size == 5 }
+puts output2
+
+
